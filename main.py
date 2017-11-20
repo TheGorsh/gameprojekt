755498e543db31fd9903e815cbfa2ab423fe73ec
Alexej98 <Alexej.Nikulin@haw-hamburg.de>
2017-11-20 11:18:21 +0100

    Init Main

---
+++ main.py
@@ -0,0 +1,23 @@
+import pygame
+
+pygame.init()
+screen = pygame.display.set_mode((800, 600))
+
+pygame.display.set_caption("Pygame-Tutorial: Grundlagen")
+pygame.mouse.set_visible(1)
+pygame.key.set_repeat(1, 30)
+
+clock = pygame.time.Clock()
+# Die Schleife, und damit unser Spiel, läuft solange running == True.
+running = 1
+while running:
+    # Framerate auf 30 Frames pro Sekunde beschränken.
+    # Pygame wartet, falls das Programm schneller läuft.
+    clock.tick(30)
+
+    # screen-Surface mit Schwarz (RGB = 0, 0, 0) füllen.
+    screen.fill((0, 0, 0))
+    for event in pygame.event.get():
+        # Spiel beenden, wenn wir ein QUIT-Event finden.
+        if event.type == pygame.QUIT:
+             running = False
